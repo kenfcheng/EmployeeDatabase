e202bd09d4e2e01597d4372aa7bb4a7ea1893844
Ken Cheng <kenfcheng@gmail.com>
2020-08-05 17:26:43 -0400

    Add Department

--- server.js
+++ server.js
@@ -187,48 +187,200 @@ function viewAllEmpByRole() {
     });
 }
 // Add employee
-function addEmp(){
-     let roleArr = [];
-     let managerArr = [];
- //Promise mysql
-     promisemysql.createConnection(connectionProperties
-     ).then((conn) => {
- 
-         return Promise.all([
-             conn.query('SELECT id, title FROM role ORDER BY title ASC'), 
-             conn.query("SELECT emp.id, concat(emp.first_name, ' ' ,  emp.last_name) AS Employee FROM employee ORDER BY Employee ASC")
-         ]);
-     }).then(([roles, managers]) => {
- 
-         for (i=0; i < roles.length; i++){
-             roleArr.push(roles[i].title);
-         }
- 
-         for (i=0; i < managers.length; i++){
-             managerArr.push(managers[i].Employee);
-         }
- 
-         return Promise.all([roles, managers]);
-     }).then(([roles, managers]) => {
- 
-         // add option for no manager
-         managerArr.unshift('--');
- 
-         inquirer.prompt([
-             {
-                 // Prompt user of their first name
-                 name: "firstName",
-                 type: "input",
-                 message: "First name: ",
-                 // Validate field is not blank
-                 validate: function(input){
-                     if (input === ""){
-                         console.log("**FIELD REQUIRED**");
-                         return false;
-                     }
-                     else{
-                         return true;
-                     }
-                 }
-             },
-             {
+function addEmp() {
+  let roleArr = [];
+  let managerArr = [];
+  //Promise mysql
+  promiseMySql
+    .createConnection(connectionProperties)
+    .then((conn) => {
+      return Promise.all([
+        conn.query("SELECT id, title FROM role ORDER BY title ASC"),
+        conn.query(
+          "SELECT emp.id, concat(emp.first_name, ' ' ,  emp.last_name) AS Employee FROM employee ORDER BY Employee ASC"
+        ),
+      ]);
+    })
+    .then(([roles, managers]) => {
+      for (i = 0; i < roles.length; i++) {
+        roleArr.push(roles[i].title);
+      }
+
+      for (i = 0; i < managers.length; i++) {
+        managerArr.push(managers[i].Employee);
+      }
+
+      return Promise.all([roles, managers]);
+    })
+    .then(([roles, managers]) => {
+      // add option for no manager
+      managerArr.unshift("--");
+
+      inquirer
+        .prompt([
+          {
+            // Prompt user of their first name
+            name: "firstName",
+            type: "input",
+            message: "First name: ",
+            // Validate field is not blank
+            validate: function (input) {
+              if (input === "") {
+                console.log("**FIELD REQUIRED**");
+                return false;
+              } else {
+                return true;
+              }
+            },
+          },
+          {
+            // Prompt user of their last name
+            name: "lastName",
+            type: "input",
+            message: "Lastname name: ",
+            // Validate field is not blank
+            validate: function (input) {
+              if (input === "") {
+                console.log("**FIELD REQUIRED**");
+                return false;
+              } else {
+                return true;
+              }
+            },
+          },
+          {
+            // User Role
+            name: "role",
+            type: "list",
+            message: "What is their role?",
+            choices: roleArr,
+          },
+          {
+            // User Manager
+            name: "manager",
+            type: "list",
+            message: "Who is their manager?",
+            choices: managerArr,
+          },
+        ])
+        .then((answer) => {
+          // Set variable for IDs
+          let roleID;
+          // Default Manager
+          let managerID = null;
+
+          // Get ID of role selected
+          for (i = 0; i < roles.length; i++) {
+            if (answer.role == roles[i].title) {
+              roleID = roles[i].id;
+            }
+          }
+
+          // get ID of selected manager
+          for (i = 0; i < managers.length; i++) {
+            if (answer.manager == managers[i].Employee) {
+              managerID = managers[i].id;
+            }
+          }
+
+          // Add employee
+          connection.query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES ("${answer.firstName}", "${answer.lastName}", ${roleID}, ${managerID})`,
+            (err, res) => {
+              if (err) return err;
+
+              console.log(
+                `\n EMPLOYEE ${answer.firstName} ${answer.lastName} ADDED...\n `
+              );
+              mainMenu();
+            }
+          );
+        });
+    });
+}
+
+// Add Role
+function addRole() {
+  //Dept. Array
+  let departmentArr = [];
+
+  promiseMySql
+    .createConnection(connectionProperties)
+    .then((conn) => {
+      return conn.query("SELECT id, name FROM department ORDER BY name ASC");
+    })
+    .then((departments) => {
+      for (i = 0; i < departments.length; i++) {
+        departmentArr.push(departments[i].name);
+      }
+
+      return departments;
+    })
+    .then((departments) => {
+      inquirer
+        .prompt([
+          {
+            // roleTitle prompt
+            name: "roleTitle",
+            type: "input",
+            message: "Role title: ",
+          },
+          {
+            // salary prompt
+            name: "salary",
+            type: "number",
+            message: "Salary: ",
+          },
+          {
+            // department prompt
+            name: "dept",
+            type: "list",
+            message: "Department: ",
+            choices: departmentArr,
+          },
+        ])
+        .then((answer) => {
+          let deptID;
+
+          for (i = 0; i < departments.length; i++) {
+            if (answer.dept == departments[i].name) {
+              deptID = departments[i].id;
+            }
+          }
+
+          // Added role
+          connection.query(
+            `INSERT INTO role (title, salary, department_id)
+                VALUES ("${answer.roleTitle}", ${answer.salary}, ${deptID})`,
+            (err, res) => {
+              if (err) return err;
+              console.log(`\n ROLE ${answer.roleTitle} ADDED...\n`);
+              mainMenu();
+            }
+          );
+        });
+    });
+}
+
+// Add Department
+function addDept() {
+  inquirer
+    .prompt({
+      // Department Name
+      name: "deptName",
+      type: "input",
+      message: "Department Name: ",
+    })
+    .then((answer) => {
+      // adds department to  table
+      connection.query(
+        `INSERT INTO department (name)VALUES ("${answer.deptName}");`,
+        (err, res) => {
+          if (err) return err;
+          console.log("\n DEPARTMENT ADDED...\n ");
+          mainMenu();
+        }
+      );
+    });
+}
