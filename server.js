510191379e791ec129750c3b46255609bba7f4f0
Ken Cheng <kenfcheng@gmail.com>
2020-08-05 11:02:06 -0400

    main menu and employee options created

--- server.js
+++ server.js
@@ -13,5 +13,39 @@ const connectionProperties = {
   database: "employees_DB",
 };
 
-// Connector
+// Connects the Database
 const connection = mysql.createConnection(connectionProperties);
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("\n EMPLOYEE TRACKER \n");
+  mainMenu();
+});
+
+
+// Main Menu
+function mainMenu(){
+
+    // User Chooses an Option
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "MAIN MENU",
+      choices: [
+        "View all employees",
+        "View employees by role",
+        "View employees by department",
+        "View employees by manager",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+        "Update employee manager",
+        "Delete employee",
+        "Delete role",
+        "Delete department",
+        "View department budgets"
+      ]
+    })
+    .then((answer) => {
